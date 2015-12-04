d4239a53a0dd778ab02900110a32f52a745d9df8
Alexey Tataurov <zealav@gmail.com>
2015-12-04 11:11:57 +0500

    feature(user): add abilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    role_id == 1
+  end
 end
