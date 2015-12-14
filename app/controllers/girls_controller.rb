4ab57c7831c221df8ab3876c0b40b674cd8678ae
Alexey Tataurov <zealav@gmail.com>
2015-12-14 11:13:43 +0500

    fix(girls): load girls, only with gallery items.

--- app/controllers/girls_controller.rb
+++ app/controllers/girls_controller.rb
@@ -2,8 +2,13 @@ class GirlsController < ApplicationController
   def index
     @girls = Girl
       .all
+      .joins('LEFT JOIN gallery_items on girls.id = gallery_items.girl_id')
+      .where('gallery_items.girl_id IS NOT NULL')
+      .group('girls.id')
       .includes(:gallery_items => [:gallery_item_image])
+      .order(:name)
       .paginate(:page => params[:page], :per_page => 48)
+
   end
 
   def show
