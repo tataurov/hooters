fe92581e5fcc0f5be264b86b0c5b39b7e9c82a57
Alexey Tataurov <zealav@gmail.com>
2015-12-14 13:55:56 +0500

    feature(seo): create robots.txt

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,4 +2,8 @@ class RootController < ApplicationController
   def index
     @girls = GalleryItem.all.sample(50)
   end
+
+  def robots
+    render "robots/#{Rails.env}", :layout => false, :content_type => 'text/plain'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   root 'root#index'
+  get '/robots.txt' => 'root#robots'
 
-  # Example of regular route:
   get 'gallery/:id' => 'gallery#index', as: :gallery
   resources :girls, only: [:index, :show]
   resource :categories, only: [:show]
-
 end
